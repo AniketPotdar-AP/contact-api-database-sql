e9cb226570de24c142be03186b75c3cd30e87f5c
Aniket Potdar <67656639+AniketPotdar-AP@users.noreply.github.com>
2023-11-11 23:26:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser")
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql"); 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
